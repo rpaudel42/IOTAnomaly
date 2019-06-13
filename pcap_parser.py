011c7385bde96951fe072470699d4adf3cb44172
muncyt <muncyt@etsu.edu>
2019-06-13 01:00:00 -0400

    Added some lines to help parse, commented out

--- pcap_parser.py
+++ pcap_parser.py
@@ -15,9 +15,6 @@ class PcapParser:
         print("\n\n..... Parsing PCAP File.....")
         pass
 
-    d = {'Time': [], 'srcIP': [], 'srcMAC': [], 'desIP': [], 'desMAC': [], 'pktSize': [], 'ports': [], 'attack': []}
-    df = pd.DataFrame(data=d)
-
     def read_pcap_file(self, filename):
 
         if not os.path.isfile(filename):
@@ -27,13 +24,35 @@ class PcapParser:
 
         packets = rdpcap(filename)
 
+        time = ["Time"]
+        srcip = ["srcIP"]
+        srcmac = ["srcMAC"]
+        desip = ["desIP"]
+        desmac = ["desMAC"]
+        pktsize = ["pktSize"]
+        ports = ["ports"]
+        attack = ["attack"]
+
+
         i = 0
         for packet in packets:
             print(packet.show())
+            #tempString = packet.show()
+            #tempList = tempString.splitlines()
+            #srcmac.append([])
+            #desmac.append([])
             print("\n\n\n")
             i += 1
+
+        #d = {'Time': time, 'srcIP': srcip, 'srcMAC': srcmac, 'desIP': desip, 'desMAC': desmac, 'pktSize': pktsize,
+        #     'ports': ports, 'attack': attack}
+        #df = pd.DataFrame(data=d)
+
         print("Total: ", i)
 
+        # df.to_csv(index=False)
+
+        # Old comments below
         # ports = [80, 25]
 
         # filtered = (pkt for pkt in pkts if
