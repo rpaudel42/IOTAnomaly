3cd5919dc5ca795140e2d5d9369f29fcb0d876cf
muncyt <muncyt@etsu.edu>
2019-06-14 02:02:43 -0400

    pushing what I have, dict syntax throws error

--- pcap_parser.py
+++ pcap_parser.py
@@ -6,7 +6,11 @@
 # 6/11/19   Paudel     Initial version,
 # ******************************************************************************
 import sys
+import time
+import argparse
+import os
 import pandas as pd
+import numpy as np
 from scapy.all import *
 
 class PcapParser:
@@ -14,6 +18,12 @@ class PcapParser:
     def __init__(self):
         print("\n\n..... Parsing PCAP File.....")
         pass
+
+    def printable_timestamp(ts, resol):
+        ts_sec = ts // resol
+        ts_subsec = ts % resol
+        ts_sec_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(ts_sec))
+        return '{}.{}'.format(ts_sec_str, ts_subsec)
 
     def read_pcap_file(self, filename):
 
@@ -33,22 +43,38 @@ class PcapParser:
         ports = ["ports"]
         attack = ["attack"]
 
-
         i = 0
         for packet in packets:
-            print(packet.show())
+
+            #print(packet.show())
+
+            count = i
+            interesting_packet_count = 0
+
+            if (packet.haslayer(IP)):
+                tempstringsrcip = packet.getlayer(IP).src
+                srcip.extend([tempstringsrcip])
+                tempstringdesip = packet.getlayer(IP).dst
+                desip.extend([tempstringdesip])
+
             #tempString = packet.show()
             #tempList = tempString.splitlines()
-            #srcmac.append([])
-            #desmac.append([])
+            #srcmac.extend([])
+            #desmac.extend([])
             print("\n\n\n")
             i += 1
 
-        #d = {'Time': time, 'srcIP': srcip, 'srcMAC': srcmac, 'desIP': desip, 'desMAC': desmac, 'pktSize': pktsize,
-        #     'ports': ports, 'attack': attack}
-        #df = pd.DataFrame(data=d)
+        d = dict(time, srcip, srcmac, desip, desmac, pktsize,
+             ports, attack)
+        df = pd.DataFrame.from_dict(d, orient="index")
+
+        print(df)
 
         print("Total: ", i)
+
+        print('{} contains {} packets ({} interesting)'.
+              format(filename, count, interesting_packet_count))
+
 
         # df.to_csv(index=False)
 
