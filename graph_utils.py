98b8b163fd016c6d552fd16eed127aba4b37d10d
Ramesh Paudel <rameshpaudel@Rameshs-MacBook-Pro.local>
2019-07-24 12:49:10 -0500

    add gbad graph file

--- graph_utils.py
+++ graph_utils.py
@@ -109,15 +109,32 @@ class GraphUtils:
             G.add_node(n, label=graph['node'][n])
         for e in graph['edge']:
             src, dest = e.split(' ')
-            # edge_count = graph['edge'][e]
+            edge_count = graph['edge'][e]
             # print("Edge:  ", graph['edge'][e])
             # if int(graph['edge'][e]) < 100:
             #     edge_count = int(graph['edge'][e])
             # else:
             #     edge_count = 100
-            # for i in  range(edge_count): #range(graph['edge'][e]):
-            G.add_edge(src, dest, label=graph['edge'][e], weight=graph['edge'][e])
+            for i in  range(edge_count): #range(graph['edge'][e]):
+                G.add_edge(src, dest, label=graph['edge'][e], weight=graph['edge'][e])
         return G
+
+    def create_gbad_file(self, g_list):
+        fw = open("gbad/iot.g", "w")
+        for g in g_list:
+            # print("G: ", g)
+            if int(g_list[g]['label']) ==  1:
+                fw.write("XN # " + str(g) + "\n")
+            else:
+                fw.write("XP # " + str(g) + "\n")
+            for n in g_list[g]['node']:
+                # print("node: ", n, g_list[g]['node'][n])
+                fw.write("v "+ str(n) + " \"" + str(g_list[g]['node'][n])+ "\"\n")
+            for e in g_list[g]['edge']:
+                # print("Edge: ", e, g_list[g]['edge'][e])
+                for i in range(0, int(g_list[g]['edge'][e])):
+                    fw.write("d " + str(e.split(' ')[0]) + " " + str(e.split(' ')[0])+ " \"c\"\n")
+
 
     def create_gml(self, g_list):
         for g in g_list:
@@ -128,7 +145,6 @@ class GraphUtils:
             for edge in g_list[g]['edge']:
                 fw.write("  edge\n  [\n     source " + str(edge.split(' ')[0]) + "\n     target "+ str(edge.split(' ')[1]) +"\n       ]\n")
             fw.write("]\n")
-
 
     def get_weighted_graph_from_csv(self, csv_file):
         print("\n\n ---- Creating Graph Files -----")
@@ -153,7 +169,7 @@ class GraphUtils:
         hour = 0
         anom_count = 0
         for index, row in firewall_log.iterrows():
-            if 1 == 1:  # row['hours_past'] < 5:
+            if  1==1: #row['time_past'] < 500:
                 if row['source'] not in global_nodes:
                     global_nodes[row['source']] = global_node_id
                     global_node_id += 1
@@ -203,3 +219,40 @@ class GraphUtils:
                 else:
                     edge[edge_id] = 1
         return g_list
+
+    def parse_streamspot(self, csv_file):
+        print("\n\n ---- Creating Edge List for Stream Spot -----")
+        firewall_log = pd.DataFrame(index=[], columns=[])
+        log = pd.read_csv(csv_file)
+        firewall_log = firewall_log.append(log)
+        firewall_log = firewall_log.iloc[:, [0, 1, 2, 3]]
+        firewall_log.columns = ['source', 'destination', 'anomaly', 'time_past']
+        firewall_log = firewall_log[firewall_log.source != '(empty)'].reset_index()
+        # print(firewall_log)
+
+        g_list = {}
+        graph = {}
+        node = {}
+        edge = []
+        label = 0
+
+        global_nodes = {}
+        local_node = {}
+        global_node_id = 1
+        local_node_id = 1
+        hour = 0
+        anom_count = 0
+        fw = open("data/iot_edges.txt", "w")
+        for index, row in firewall_log.iterrows():
+            if 1 == 1:  # row['time_past'] < 5: #1 == 1: #
+                # if row['time_past'] % 50 == 0:
+                #     print("Graph   ", row['time_past'])
+                if row['source'] not in global_nodes:
+                    global_nodes[row['source']] = global_node_id
+                    global_node_id += 1
+
+                if row['destination'] not in global_nodes:
+                    global_nodes[row['destination']] = global_node_id
+                    global_node_id += 1
+
+                fw.write(str(global_nodes[row['source']])+ "\tx\t" + str(global_nodes[row['destination']]) + "\tx\t1\t"+ str(row['time_past'])+"\n")
